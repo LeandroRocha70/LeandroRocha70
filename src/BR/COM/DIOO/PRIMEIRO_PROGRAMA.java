3af4044a72e286689df0756da0154e750b626589
LeandroRocha70 <leandro.masters@hotmail.com>
2022-01-21 18:05:58 -0300

    Dominação2

---
+++ src/BR/COM/DIOO/PRIMEIRO_PROGRAMA.java
@@ -0,0 +1,19 @@
+package BR.COM.DIOO;
+
+import BR.COM.DIOO.MODEL.Gato;
+
+public class PRIMEIRO_PROGRAMA {
+
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        
+       /* int a = 5;
+        int b =3;
+
+        System.out.println("Hello World " + (a+b));*/
+
+    }
+}
